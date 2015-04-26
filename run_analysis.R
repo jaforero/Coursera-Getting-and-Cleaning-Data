ce9c15c6f62492b8fee68035814009d85d718e79
Javier Forero <git@javierforero.com>
2015-04-26 18:20:42 -0500

    Update run_analysis.R

--- run_analysis.R
+++ run_analysis.R
@@ -1,10 +1,9 @@
 run_analysis.R
 
 # Source of data for this project: https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip
-
 # This R script does the following:
 
-# 1. Merges the training and the test sets to create one data set.
+# 1. Merges the training and the test sets to create one data set
 
 tmp1 <- read.table("train/X_train.txt")
 tmp2 <- read.table("test/X_test.txt")
@@ -18,7 +17,7 @@ tmp1 <- read.table("train/y_train.txt")
 tmp2 <- read.table("test/y_test.txt")
 Y <- rbind(tmp1, tmp2)
 
-# 2. Extracts only the measurements on the mean and standard deviation for each measurement.
+# 2. Extracts only the measurements on the mean and standard deviation for each measurement
 
 features <- read.table("features.txt")
 indices_of_good_features <- grep("-mean\\(\\)|-std\\(\\)", features[, 2])
@@ -27,20 +26,20 @@ names(X) <- features[indices_of_good_features, 2]
 names(X) <- gsub("\\(|\\)", "", names(X))
 names(X) <- tolower(names(X))
 
-# 3. Uses descriptive activity names to name the activities in the data set.
+# 3. Uses descriptive activity names to name the activities in the data set
 
 activities <- read.table("activity_labels.txt")
 activities[, 2] = gsub("_", "", tolower(as.character(activities[, 2])))
 Y[,1] = activities[Y[,1], 2]
 names(Y) <- "activity"
 
-# 4. Appropriately labels the data set with descriptive activity names.
+# 4. Appropriately labels the data set with descriptive activity names
 
 names(S) <- "subject"
 cleaned <- cbind(S, Y, X)
 write.table(cleaned, "merged_clean_data.txt")
 
-# 5. Creates a 2nd, independent tidy data set with the average of each variable for each activity and each subject.
+# 5. From the data set in step 4, creates a second, independent tidy data set with the average of each variable for each activity and each subject
 
 uniqueSubjects = unique(S)[,1]
 numSubjects = length(unique(S)[,1])
